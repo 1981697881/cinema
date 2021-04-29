7d4c0f42a85094834e24426caa15898e052dc680
1981697881 <1981697881@qq.com>
2021-04-29 17:26:31 +0800

    0.3.9

--- src/views/extension/groupTicket/form/DetailInfo.vue
+++ src/views/extension/groupTicket/form/DetailInfo.vue
@@ -1,5 +1,16 @@
 <template>
   <div>
+    <el-row :gutter="20" id="all1" class="printClass">
+      <div class="rowClass">
+        <div class="pdfClass" v-for="(item,index) in multipleSelection" :key="index">
+          <img
+            style="width: 14.5cm; height: 6.62cm"
+            src="@/assets/img/_20210428205913.jpg"
+            fit="fit"/>
+          <div class="scanImg" :id="'qrCode'+index"></div>
+        </div>
+      </div>
+    </el-row>
     <el-form ref="form" label-width="100px" :size="'mini'">
       <!--<el-row :gutter="20">
         <el-col :span="10">
@@ -11,70 +22,7 @@
           <el-button  :size="'mini'" type="success" @click="query" icon="el-icon-search">查询</el-button>
         </el-col>
       </el-row>-->
-      <el-row :gutter="20" id="all1" class="printClass">
-        <div class="pdfClass">
-        <img
-          style="width: 14.5cm; height: 5.61cm"
-          src="@/assets/img/_20210428205913.jpg"
-          fit="fit"/>
-          <img
-            class="scanImg"
-            style="width: 2cm; height: 2cm"
-            src="@/assets/img/erweima.png"
-            fit="fit" />
-          </div><div class="pdfClass">
-        <img
-          style="width: 14.5cm; height: 5.61cm"
-          src="@/assets/img/_20210428205913.jpg"
-          fit="fit"/>
-          <img
-            class="scanImg"
-            style="width: 2cm; height: 2cm"
-            src="@/assets/img/erweima.png"
-            fit="fit" />
-          </div> <div class="pdfClass">
-        <img
-          style="width: 14.5cm; height: 5.61cm"
-          src="@/assets/img/_20210428205913.jpg"
-          fit="fit"/>
-          <img
-            class="scanImg"
-            style="width: 2cm; height: 2cm"
-            src="@/assets/img/erweima.png"
-            fit="fit" />
-          </div><div class="pdfClass">
-        <img
-          style="width: 14.5cm; height: 5.61cm"
-          src="@/assets/img/_20210428205913.jpg"
-          fit="fit"/>
-          <img
-            class="scanImg"
-            style="width: 2cm; height: 2cm"
-            src="@/assets/img/erweima.png"
-            fit="fit" />
-          </div> <div class="pdfClass">
-        <img
-          style="width: 14.5cm; height: 5.61cm"
-          src="@/assets/img/_20210428205913.jpg"
-          fit="fit"/>
-          <img
-            class="scanImg"
-            style="width: 2cm; height: 2cm"
-            src="@/assets/img/erweima.png"
-            fit="fit" />
-          </div><div class="pdfClass">
-        <img
-          style="width: 14.5cm; height: 5.61cm"
-          src="@/assets/img/_20210428205913.jpg"
-          fit="fit"/>
-          <img
-            class="scanImg"
-            style="width: 2cm; height: 2cm"
-            src="@/assets/img/erweima.png"
-            fit="fit" />
-          </div>
 
-      </el-row>
       <el-row :gutter="20">
         <el-col :span="24">
           <el-table class="list-main" height="250px" :data="list" border size="mini" :highlight-current-row="true" @selection-change="handleSelectionChange">
@@ -105,6 +53,7 @@
   import {addMovier,getShareList} from "@/api/extension/index";
   import html2canvas from 'html2canvas';
   import jspdf from 'jspdf';
+  import QRCode from 'qrcodejs2'
   import {
     getToken
   } from '@/utils/auth'
@@ -140,8 +89,26 @@
       }
     },
     methods: {
+      creatQrCode(element,val) {
+        var qrcode = new QRCode(element, {
+          text: val, // 需要转换为二维码的内容
+          width: 100,
+          height: 100,
+          colorDark: '#000000',
+          colorLight: '#ffffff',
+          correctLevel: QRCode.CorrectLevel.H
+        })
+      },
       handleSelectionChange(val) {
+        let that = this
         this.multipleSelection = val;
+        this.$nextTick(() => {
+
+
+        this.multipleSelection.forEach((item,index)=>{
+          that.creatQrCode('qrCode'+index,item.memberCdkeyShare)
+        })
+        })
       },
       downPdf() {
         window.scrollTo(0, 0) //注意这里必须设置为顶部不然会出现图片不全
@@ -152,28 +119,27 @@
           var contentWidth = canvas.width;
           var contentHeight = canvas.height;
           //一页pdf显示html页面生成的canvas高度;
-          var pageHeight = contentWidth;
+          var pageHeight = contentWidth /841.89  * 592.28;
           //未生成pdf的html页面高度
           var leftHeight = contentHeight;
-          //pdf页面偏移
+          //页面偏移
           var position = 0;
           //a4纸的尺寸[595.28,841.89]，html页面生成的canvas在pdf中图片的宽高
-          var imgWidth = 585.28;
-          var imgHeight = 841.89;
-
-          var pageData = canvas.toDataURL('image/jpeg', 1.0);
-          var pdf = new jspdf('', 'pt', 'a4');
+          var imgWidth = 841.89;
+          var imgHeight = 841.89/contentWidth * contentHeight;
+          var pageData = canvas.toDataURL();
+          var pdf = new jspdf('l', 'pt', 'a4');
           //有两个高度需要区分，一个是html页面的实际高度，和生成pdf的页面高度(841.89)
           //当内容未超过pdf一页显示的范围，无需分页
           if (leftHeight < pageHeight) {
-            pdf.addImage(pageData, 'JPEG', 5, 10, imgWidth, imgHeight);
-          } else {
-            while (leftHeight > 0) {
-              pdf.addImage(pageData, 'JPEG', 5, position, imgWidth, imgHeight)
+            pdf.addImage(pageData, 'JPEG', 0, 0, imgWidth, imgHeight );
+          }else {
+            while(leftHeight > 0) {
+              pdf.addImage(pageData, 'JPEG', 0, position, imgWidth, imgHeight)
               leftHeight -= pageHeight;
-              position -= 841.89;
+              position -= 592.28;
               //避免添加空白页
-              if (leftHeight > 0) {
+              if(leftHeight > 0) {
                 pdf.addPage();
               }
             }
@@ -204,34 +170,47 @@
     margin-left: 10px;
   }
   .printClass{
+    position: fixed;
+    z-index: 99;
+    top: -99999999999999px;
     margin-right: 0 !important;
     margin-left: 0 !important;
     heihgt: 21cm;
     width: 29.7cm;
-    margin-top: 7cm;
-    display: flex;
-    flex-wrap: wrap;
     /*position: fixed;
     z-index: 99;
     left: 10%;*/
-    transform:rotate(90deg);
-    -ms-transform:rotate(90deg); 	/* IE 9 */
-    -moz-transform:rotate(90deg); 	/* Firefox */
-    -webkit-transform:rotate(90deg); /* Safari 和 Chrome */
-    -o-transform:rotate(90deg); 	/* Opera */
+
 
   }
   .scanImg{
     position: absolute;
-    right:0.3cm;
+    right:0.4cm;
     bottom: 0.3cm;
     z-index: 101;
+    display: inline-block;
+
   }
+  .scanImg img{
+    width: 2.5cm; height: 2.5cm;
+    background-color: #fff;
+    padding: 6px;
+    box-sizing: border-box;
+  }
+  .rowClass{
+    display: flex;
+    flex-wrap: wrap;
+  }
+  .rowClass:first-child{
+  }
+
   .pdfClass{
     margin: 5px;
     position: relative;
 
   }
+
+
   .button-new-tag {
     margin-left: 10px;
     height: 32px;
